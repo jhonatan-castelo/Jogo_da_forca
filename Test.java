f1ba20f5525093b1c5727c7c1da1250dafb6b7db
jhonatan-castelo <39504906+jhonatan-castelo@users.noreply.github.com>
2018-05-28 21:12:59 -0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
-package test;
+package asfd;
 
 import java.util.Scanner;
+import java.security.*;
 
 interface StringTest {
 	boolean test(String aStr, String bStr);
@@ -11,7 +12,7 @@ public class Test {
 		
 		//System.out.println(palavras);
 		
-		StringTest isIn = (animais, palavras) -> animais.indexOf(palavras) != -1;
+		//StringTest isIn = (animais, palavras) -> animais.indexOf(palavras) != -1;
 		/*
 		String str = "This is a test";
 		
@@ -27,37 +28,43 @@ public class Test {
 		else
 			System.out.println("'xyz' not found");
 		*/	
-		String animais = "Girafa";
-		char[] traco = new char[animais.length()];
+		String[] animais = {"girafa","cachorro","gato","pato","morcego","lobo"};
+		SecureRandom random = new SecureRandom();
 		
-		String enc = animais.substring(0, 1);
+		int r = random.nextInt(animais.length);
+		System.out.println(r);
+		
+		
+		char[] traco = new char[animais[r].length()];
+		
+		//String enc = animais.substring(0, 1);
 		//System.out.println(enc);
 		
-
-		for( int i = 0; i < animais.length(); i++) {
-			traco[i] = '_';
-			System.out.print(traco[i]+" ");		
-		}
-		for (int j = 0; j < 10; j++) {
-			
-			System.out.println("\nDigite uma letra: ");
-			char palavras = teclado.next().charAt(0);
-			
-			for( int i = 0; i < animais.length(); i++) {
-				
-				if( palavras == animais.charAt(i)) {
-					traco[i] = palavras;
-					
-				}
-				//System.out.print(animais.charAt(i));
-				/*if (isIn.test(animais, palavras)) {
-					traco[i] = palavras;
-				}*/
-				System.out.print(traco[i] + " ");
-				
+			for( int i = 0; i < animais[r].length(); i++) {
+				traco[i] = '_';
+				System.out.print(traco[i]+" ");		
 			}
 			
-		}
+			for (int j = 0; j < 6; j++) {
+				
+				System.out.println("\nDigite uma letra: ");
+				char palavras = teclado.next().charAt(0);
+				
+				for( int i = 0; i < animais[r].length(); i++) {
+					
+					if( palavras == animais[r].charAt(i)) {
+						traco[i] = palavras;
+						
+					}
+					//System.out.print(animais.charAt(i));
+					/*if (isIn.test(animais, palavras)) {
+						traco[i] = palavras;
+					}*/
+					System.out.print(traco[i] + " ");
+					
+				}
+				
+			}
 	}
 
 }
