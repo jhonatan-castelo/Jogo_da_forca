7fa05404bed20a5dff008c53f187ac4281fd5612
VictorBSI <38796125+VictorBSI@users.noreply.github.com>
2018-06-11 00:57:44 -0300

    Update Jogo.java

--- Jogo.java
+++ Jogo.java
@@ -1,16 +1,17 @@
-package forca;
+package jogo;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.security.*;
 
-/*interface StringTest {
-	boolean test(String aStr, String bStr);
-}*/
 
 public class Jogo implements Resultados{
-		
+		boolean achou = true;
+		boolean erro;
 		Jogador[] player;
 		Roleta row;
+		String[] corpo = {"Cabeça", "Tronco", "Braço esquerdo", "Braço direito", "Perna Esquerda", "Perna Direita"};
+		int acerto = 0, cont = 0;
 		
 		public void iniciarJogo(Jogador player) {
 			this.player = new Jogador[2];
@@ -19,77 +20,64 @@ public class Jogo implements Resultados{
 		public void terminarJogo(Jogador player) {
 			
 		}
-		
-		
-		
-		//System.out.println(palavras);
-		
-		//StringTest isIn = (animais, palavras) -> animais.indexOf(palavras) != -1;
-		/*
-		String str = "This is a test";
-		
-		System.out.println("Testing string: " + str);
-		
-		if(isIn.test(str, "is a"))
-			System.out.println("'is a' found.");
-		else
-			System.out.println("'is a' not found");
-		
-		if(isIn.test(str, "xyz"))
-			System.out.println("'xyz' Found");
-		else
-			System.out.println("'xyz' not found");
-		*/	
-		
 	
 
 	@Override
 	public void mostrarResultados() {
-		//Roleta r = new Roleta();
-		Scanner teclado = new Scanner(System.in);
-		// TODO Auto-generated method stub
-		String[] animais = {"girafa","cachorro","gato","pato","morcego","lobo"};
-		SecureRandom random = new SecureRandom();
 		
-		int r = random.nextInt(animais.length);
-		System.out.println(r);
-		
-		
-		char[] traco = new char[animais[r].length()];
-		
-		//String enc = animais.substring(0, 1);
-		//System.out.println(enc);
-		
-			for( int i = 0; i < animais[r].length(); i++) {
-				traco[i] = '_';
-				System.out.print(traco[i]+" ");		
-			}
-			
-			for (int j = 0; j < 6; j++) {
-				
-				System.out.println("\nDigite uma letra: ");
-				char palavras = teclado.next().charAt(0);
-				
-				for( int i = 0; i < animais[r].length(); i++) {
-					
-					if( palavras == animais[r].charAt(i)) {
-						traco[i] = palavras;
-						
-					}
-					//System.out.print(animais.charAt(i));
-					/*if (isIn.test(animais, palavras)) {
-						traco[i] = palavras;
-					}*/
-					System.out.print(traco[i] + " ");
-					
-				}
-				
-			}
 	}
 	public void jogar(Jogador player) {
 		
 	}
 	
-	
+	public void Jogar(String animais) {
+			Scanner teclado = new Scanner(System.in);
 
+			char[] traco = new char[animais.length()];;
+			
+				for( int i = 0; i < animais.length(); i++) {
+					traco[i] = '_';
+					System.out.print(traco[i]+" ");		
+				}
+				while(achou) {
+					erro = true;
+					System.out.println("\nDigite uma letra: ");
+					char palavras = teclado.next().charAt(0);
+					
+					
+					for( int i = 0; i < animais.length(); i++) {
+						
+						if (palavras == traco[i]) {
+							System.out.print(traco[i]+" ");
+							continue;
+						}
+						if( palavras == animais.charAt(i)) {
+							traco[i] = palavras;
+							acerto++;
+							erro = false;
+							
+						}
+						System.out.print(traco[i]+ " ");	
+					}
+					if (erro == true) {
+						System.out.println("\n\n"+corpo[cont]);
+						cont++;
+
+					}
+					if (cont == 6) {
+						System.out.println("\n\nVocê perdeu!");
+						achou = false;
+					}
+
+					if(acerto == animais.length()) {
+						System.out.println("\n\nVocê ganhou!");
+						achou = false;
+					}
+					
+				}
+				
+				
+					
+				
+	}
 }
