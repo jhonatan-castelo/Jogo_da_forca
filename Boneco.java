523bd43afbb2268e3c18369bdaaeeee7203fd42a
jhonatan-castelo <39504906+jhonatan-castelo@users.noreply.github.com>
2018-06-13 23:26:29 -0300

    atualização da classe boneco
    boneco genérico

--- Boneco.java
+++ Boneco.java
@@ -1,5 +1,33 @@
 package forca;
 
-public class Boneco {
+public class Boneco <T>{
+	T cabeca;
+	T tronco;
+	T p_esq;
+	T p_drt;
+	T b_esq;
+	T b_drt;
 
+	public void setCabeca(T cabeca) {
+		this.cabeca = cabeca;
+	}
+
+	public void setTronco(T tronco) {
+		this.tronco = tronco;
+	}
+
+	public void setP_esq(T p_esq) {
+		this.p_esq = p_esq;
+	}
+	public void setP_drt(T p_drt) {
+		this.p_drt = p_drt;
+	}
+	public void setB_esq(T b_esq) {
+		this.b_esq = b_esq;
+	}
+	public void setB_drt(T b_drt) {
+		this.b_drt = b_drt;
+	}
+
+	
 }
